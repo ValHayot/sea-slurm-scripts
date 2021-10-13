6291bc638cfc25c8360a097b1da01da0b988693c
valhayot <valeriehayot@gmail.com>
2021-10-12 22:16:39 -0400

    uncommented some experiments

--- sea_spm/launch_all.sh
+++ sea_spm/launch_all.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-    #"bash sbatch_spm.sh 1 1 ds001545 0" 
-    #"bash sbatch_spm.sh 8 8 ds001545 0" 
-    #"bash sbatch_spm.sh 16 16 ds001545 0" 
-    #"bash sbatch_spm.sh 1 1 ds001545 6" 
 declare -a exp=( 
+    "bash sbatch_spm.sh 1 1 ds001545 0" 
+    "bash sbatch_spm.sh 8 8 ds001545 0" 
+    "bash sbatch_spm.sh 16 16 ds001545 0" 
     "bash sbatch_spm.sh 1 1 preventad 0" 
     "bash sbatch_spm.sh 8 8 preventad 0" 
     "bash sbatch_spm.sh 16 16 preventad 0" 
     "bash sbatch_spm.sh 1 1 hcp 0" 
     "bash sbatch_spm.sh 8 8 hcp 0" 
     "bash sbatch_spm.sh 16 16 hcp 0" 
+    "bash sbatch_spm.sh 1 1 ds001545 6" 
     "bash sbatch_spm.sh 8 8 ds001545 6" 
     "bash sbatch_spm.sh 16 16 ds001545 6" 
     "bash sbatch_spm.sh 1 1 preventad 6" 
